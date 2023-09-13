0319688e1b2492e764785c859f62d753e550f4dc
gbaer <gbaer@EYI-PC>
2023-09-13 01:40:25 -0400

    first commit

---
+++ src/NewTodoForm.js
@@ -0,0 +1,36 @@
+/** New Todo Form Component */
+
+import { useState } from "react";
+import { v4 as uuid } from "uuid";
+
+function NewTodoForm({ createTodo }) {
+  const [task, setTask] = useState("");
+
+  const handleChange = (e) => {
+    setTask(e.target.value);
+  };
+
+  const gatherInput = (e) => {
+    e.preventDefault();
+    createTodo({ task, id: uuid() });
+    setTask("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={gatherInput}>
+        <label htmlFor="task">Task:</label>
+        <input
+          id="task"
+          name="task"
+          type="text"
+          onChange={handleChange}
+          value={task}
+        />
+        <button>Add a Todo</button>
+      </form>
+    </div>
+  );
+}
+
+export default NewTodoForm;
